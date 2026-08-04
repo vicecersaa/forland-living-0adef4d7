f34693e56d1a04063939df6b8ff2abc73c5e0762
vicecersa <vicecersaa@gmail.com>
2026-08-04 23:38:21 +0700

    adding favicon and updating meta

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -110,7 +110,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/favicon.jpeg", type: "image/x-icon" },
     ],
   }),
   shellComponent: RootShell,
